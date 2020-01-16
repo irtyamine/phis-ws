9583cd410c9b7a046274df61ba598caafe0d3d42
a-iratni <54934103+a-iratni@users.noreply.github.com>
2020-01-16 08:48:22 +0100

    Update userTest in order to break test

--- phis2-ws/src/test/java/opensilex/service/userTest.java
+++ phis2-ws/src/test/java/opensilex/service/userTest.java
@@ -105,7 +105,7 @@ public class userTest extends JerseyTest {
 
         Response response = target("brapi/v1/token").request().post(userEntity); //Here we send POST request
 
-        assertEquals("should return status 201 ( = CREATED)", 201, response.getStatus());
+        assertEquals("should return status 201 ( = CREATED)", 200, response.getStatus());
         assertNotNull("Should return TOKEN", response.getEntity().toString());
 
         TokenResponseStructure token = response.readEntity(TokenResponseStructure.class);
